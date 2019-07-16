cf2eff950a3cc56f335a203698c3bbb1d51775e6
lim <lim@decent.cn>
2019-07-16 17:17:31 +0800

    修改

--- src/index.js
+++ src/index.js
@@ -3,19 +3,25 @@ import { alltotal, dataSI } from './js/baseData';
 let dom = document.getElementById("container");
 let myChart = echarts.init(dom);
 let option = null;
-let dateList = ['2019-07-01', '2019-07-02', '2019-07-03','2019-07-04', '2019-07-05', '2019-07-06', '2019-07-07'];
+let dateList = ['2019-07-01', '2019-07-02', '2019-07-03', '2019-07-04', '2019-07-05', '2019-07-06', '2019-07-07'];
 let optionsData = [];
 dateList.forEach((element, index) => { //options数据
     let key = index + 1;
     optionsData.push({
-        title: { text:  element + '详细数据' },
-        series: [
-            { data: dataSI[key] },
+        title: {
+            text: element + '详细数据'
+        },
+        series: [{
+                data: dataSI[key]
+            },
             {
-                data: [
-                    { name: '已完成', value: dataSI[key + 'sum'] },
-                    { name: '未完成', value: alltotal - dataSI[key + 'sum'] }
-                ]
+                data: [{
+                    name: '已完成',
+                    value: dataSI[key + 'sum']
+                }, {
+                    name: '未完成',
+                    value: alltotal - dataSI[key + 'sum']
+                }]
             }
         ]
     })
@@ -42,8 +48,10 @@ option = {
         },
         calculable: true,
         grid: {
-            top: 80,
+            top: 60,
             bottom: 100,
+            width: '70%',
+            left: 50
         },
         tooltip: {
             trigger: 'axis',
@@ -71,7 +79,7 @@ option = {
                     '高新万达一店', '宽厚里一店', '泺源大街店', '普利街店', '汇隆广场店', '滨河码头店', '滨河物流店', '华信店', '高新万达二店', '汉峪金谷一店',
                     '大明湖店', '齐盛广场一店', '齐盛广场二店', '历黄路店', '未来城店', '诚信大厦店', '汉峪金谷一店', '文化西路店', '山大北路店', '荣盛国际店',
                     '山东电视台店', '花园路店', '高新万达三店', '绿地缤纷城店', '和谐广场店', '中润广场店', '高新万达旗舰店', '宽厚里三店', '宽厚里二店', '洪楼广场店',
-                    '报业大厦店', '绿城金融中心店', '林华大厦店', '乐梦中心店', '芙蓉街店', '汉峪金谷二店', '', '', '', '', '', '', '', ''
+                    '报业大厦店', '绿城金融中心店', '林华大厦店', '乐梦中心店', '芙蓉街店', '汉峪金谷二店'
                 ],
                 splitLine: { show: false },
                 axisLabel: {//坐标轴刻度标签的相关设置。
@@ -85,8 +93,11 @@ option = {
             name: '完成率%',
             axisLabel: {
                 formatter: function (value, index, d) {
-                    console.log(value);
-                    return value.toFixed(2) * 100 + '%'
+                    console.log(value.toFixed(2));
+                    console.log(value.toFixed(2) * 100)
+                    console.log(value.toFixed(2) * 100 + '%');
+                    var dataVal = value * 100;
+                    return dataVal.toFixed(0) + '%'
                 }
             },
         },
